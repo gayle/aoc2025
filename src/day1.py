8a69f85245ad1bd69dbc41e8c136296015959212
Gayle <gayle_999@yahoo.com>
2025-12-10 14:46:23 -0500

    add code to be able to run by command line

--- src/day1.py
+++ src/day1.py
@@ -29,3 +29,12 @@ class Day1:
 			new_position = (starting_position - 1mount) % 100
 		else: 
 			raise ValueError("invalid direction; expected 'L' or 'R'")
+
+if __name__ == "__main__":
+	import sys
+	if len(sys.args)!= 2:
+		print("Usage: python day1.py <input_filename>")
+		sys.exit(1)
+	input_filename = sys.argv[1]
+	result = Day1.count_times_dial_is_at_zero(input_filename)
+	print(result)
