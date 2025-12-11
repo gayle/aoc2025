deb2e2fe296f86429854963b6d72c4304c9a9380
Gayle <gayle_999@yahoo.com>
2025-12-11 15:54:45 -0500

    Implemented Day1 Part 2
    and changed Day2 to be separate files, instead of refactoring Day1 code to do Day2 instead 
    So that we can compare stuff.

---
+++ test/day1_pt2.py
@@ -0,0 +1,53 @@
+class Day1Pt2:
+
+    @staticmethod
+    def count_times_dial_crosses_zero(input_filename):
+        times_dial_crosses_zero = 0
+        content = ''
+        with open(input_filename, 'r') as f:
+            content = f.read()
+
+        lines = content.split()
+        dial_position = 50 # Starting position
+        for line in lines:
+            encountered_zero, dial_position = Day1Pt2.rotate_dial(dial_position, line)
+            times_dial_crosses_zero += encountered_zero
+        return times_dial_crosses_zero
+
+    @staticmethod
+    def rotate_dial(starting_position, action):
+        if starting_position < 0 or starting_position > 99:
+            raise ValueError("Starting position must be between 1 and 99")
+        direction = action[0]
+        amount = int(action[1:])
+        times_dial_crosses_zero=0
+        if direction == "R":
+            new_position = starting_position + amount
+            times_dial_crosses_zero, dial_position  = divmod(new_position, 100)
+        elif direction == "L":
+            new_position = starting_position - amount
+            dial_position = new_position % 100
+
+            # THIS WORKS
+            import math
+            times_dial_crosses_zero = math.trunc(abs(new_position / 100))
+            if (starting_position > 0 and new_position <= 0): times_dial_crosses_zero += 1
+
+            # THE ABOVE WORKS BUT WHY CAN'T I USE DIVMOD HERE?
+            # times_dial_crosses_zero, dial_position  = divmod(new_position, 100)
+            # times_dial_crosses_zero = abs(times_dial_crosses_zero)
+            # if (starting_position > 0 and new_position <= 0): times_dial_crosses_zero += 1
+
+        else:
+            raise ValueError("Invalid direction; expected 'L' or 'R'")
+        return times_dial_crosses_zero, dial_position
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python day1_pt2.py <input_filename>")
+        sys.exit(1)
+    input_filename = sys.argv[1]
+    (result1, result2) = Day1Pt2.count_times_dial_crosses_zero(input_filename)
+    print(f"part1 result:{result1}")
+    print(f"part2 result:{result2}")
---
+++ test/test_day1_pt2.py
@@ -0,0 +1,94 @@
+import pytest
+import tempfile
+import sys, os
+sys.path.append(os.path.dirname('../src/'))
+from day1_pt2 import Day1Pt2
+
+def test_right_rotation():
+    assert (0,12) == Day1Pt2.rotate_dial(5, "R7")
+
+def test_right_rotation_pass_99():
+    assert (1,55) == Day1Pt2.rotate_dial(95, "R60")
+
+def test_right_rotation_start_at_0():
+    assert (0,14) == Day1Pt2.rotate_dial(0, "R14")
+
+def test_right_rotation_end_at_0():
+    assert (1,0) == Day1Pt2.rotate_dial(52, "R48")
+
+def test_right_rotation_large_number():
+    assert (10,50) == Day1Pt2.rotate_dial(50, "R1000")
+
+def test_left_rotation():
+    assert (0,52) == Day1Pt2.rotate_dial(82, "L30")
+
+def test_left_rotation_pass_0():
+    assert (1,82) == Day1Pt2.rotate_dial(50, "L68")
+
+def test_left_rotation_start_at_0():
+    assert (0,95) == Day1Pt2.rotate_dial(0, "L5")
+
+def test_left_rotation_end_at_0():
+    assert (1,0) == Day1Pt2.rotate_dial(55, "L55")
+
+def test_left_rotation_large_number():
+    assert (5,0) == Day1Pt2.rotate_dial(50, "L450")
+
+def test_left_rotation_larger_number():
+    assert (10,50) == Day1Pt2.rotate_dial(50, "L1000")
+
+def test_invalid_input_starting_position_negative():
+    try:
+        Day1Pt2.rotate_dial(-1, "R10")
+        assert False, "Starting position must be between 0 and 99"
+    except ValueError:
+        pass
+
+def test_invalid_input_starting_position_greater_than_99():
+    try:
+        Day1Pt2.rotate_dial(100, "R10")
+        assert False, "Starting position must be between 0 and 99"
+    except ValueError:
+        pass
+
+def test_invalid_input_letter():
+    try:
+        Day1Pt2.rotate_dial(50, "X10")
+        assert False, "Invalid direction; expected 'L' or 'R'"
+    except ValueError:
+        pass
+
+def test_count_times_dial_crosses_zero():
+    test_content = """L68
+    L30
+    R48
+    L5
+    R60
+    L55
+    L1
+    L99
+    R14
+    L82"""
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as tmp:
+        tmp.write(test_content)
+        tmp.flush()  # Ensure data is written
+        temp_filename = tmp.name
+
+    try:
+        result = Day1Pt2.count_times_dial_crosses_zero(temp_filename)
+        assert 6 == result
+    finally:
+        os.remove(temp_filename)
+
+def test_count_times_dial_crosses_zero_with_R1000():
+    test_content = "R1000"
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as tmp:
+        tmp.write(test_content)
+        tmp.flush()  # Ensure data is written
+        temp_filename = tmp.name
+
+    try:
+        result = Day1Pt2.count_times_dial_crosses_zero(temp_filename)
+        assert 10 == result
+    finally:
+        os.remove(temp_filename)
