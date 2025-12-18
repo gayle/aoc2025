f4125d7110ffd200ddf11949937d8f1e9bd5470c
Dean Stautberg <dstautberg@gmail.com>
2025-12-17 21:13:15 -0500

    Got day 2 part 1 working

--- day2_part1.py
+++ day2_part1.py
@@ -1,21 +1,48 @@
+import sys
+
 class Day2Part1:
 
     @staticmethod
     def parse_input(input_filename):
         input = open(input_filename).read()
         ranges = input.split(",")
+        input_ranges = []
         for range in ranges:
             start, end = range.split("-")[0:2]
             start, end = int(start), int(end)
-            print(f"Range: {start} to {end}")                
+            input_ranges.append((start, end))
+        return input_ranges
+    
+    @staticmethod
+    def invalid_ids(start, end):
+        invalid_ids = []
+        for n in range(start, end + 1):
+            sn = str(n)
+            # print("sn: ", sn)
+            length = len(sn)
+            # print("length: ", length)
+            halfway = int(length/2)
+            # print("sn[0:halfway]: ", sn[0:halfway])
+            # print("sn[halfway:length]: ", sn[halfway:length])
+            if sn[0:halfway] == sn[halfway:length]:
+                # print("Invalid ID:", n)
+                invalid_ids.append(n)
+        # print("invalid_ids: ", invalid_ids)
+        return invalid_ids
 
     @staticmethod
-    def contains_invalid_id(start, end):
-        for i in range(start, end + 1):
-            s = str(i)
-            # print(S)
-            for c in s:
-                # Detect if s consists of two duplicate sets of numbers, like 123123
-                pass
-        return True
-            
+    def sum_of_invalid_ids(input_filename):
+        input_ranges = Day2Part1.parse_input(input_filename)
+        total = 0
+        for i, j in input_ranges:
+            invalid_ids = Day2Part1.invalid_ids(i, j)
+            total += sum(invalid_ids)
+        return total
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python day2_pt1.py <input_filename>")
+        sys.exit(1)
+    input_filename = sys.argv[1]
+    result = Day2Part1.sum_of_invalid_ids(input_filename)
+    print(f"Day 2 Part 1 result: {result}")
--- test_day2_part1.py
+++ test_day2_part1.py
@@ -3,5 +3,54 @@ import tempfile
 import sys, os
 from day2_part1 import Day2Part1
 
-def test_right_rotation():
-    assert True == Day2Part1.contains_invalid_id(1, 2)
+# Sample from the problem description page:
+
+# 11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+# 1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+# 824824821-824824827,2121212118-2121212124
+
+# 11-22 has two invalid IDs, 11 and 22.
+# 95-115 has one invalid ID, 99.
+# 998-1012 has one invalid ID, 1010.
+# 1188511880-1188511890 has one invalid ID, 1188511885.
+# 222220-222224 has one invalid ID, 222222.
+# 1698522-1698528 contains no invalid IDs.
+# 446443-446449 has one invalid ID, 446446.
+# 38593856-38593862 has one invalid ID, 38593859.
+# The rest of the ranges contain no invalid IDs.
+
+# Adding up all the invalid IDs in this example produces 1227775554.
+
+def test_11_22():
+    assert [11, 22] == Day2Part1.invalid_ids(11, 22)
+
+def test_95_115():
+    assert [99] == Day2Part1.invalid_ids(95, 115)
+
+def test_998_1012():
+    assert [1010] == Day2Part1.invalid_ids(998, 1012)
+
+def test_1188511880_1188511890():
+    assert [1188511885] == Day2Part1.invalid_ids(1188511880, 1188511890)
+
+def test_222220_222224():
+    assert [222222] == Day2Part1.invalid_ids(222220, 222224)
+
+def test_1698522_1698528():
+    assert [] == Day2Part1.invalid_ids(1698522, 1698528)
+
+def test_446443_446449():
+    assert [446446] == Day2Part1.invalid_ids(446443, 446449)
+
+def test_38593856_38593862():
+    assert [38593859] == Day2Part1.invalid_ids(38593856, 38593862)
+
+def test_565653_565659():
+    assert [] == Day2Part1.invalid_ids(565653, 565659)
+
+def test_824824821_824824827():
+    assert [] == Day2Part1.invalid_ids(824824821, 824824827)
+
+def test_2121212118_2121212124():
+    assert [] == Day2Part1.invalid_ids(2121212118, 2121212124)
+
